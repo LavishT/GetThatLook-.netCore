0790b1ada19e34068ab45ea3ad9147f63211abb7
LavishT <lavishtalwar28@github.com>
2020-06-12 10:06:17 -0400

    Created About Us and categories and linked them from home page..

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -27,6 +27,14 @@ namespace GetThatLook.Controllers
         {
             return View();
         }
+        public IActionResult Category() 
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
